acbce419a6f0eae5bd12988a7e58eb897fbe2501
Jasmine Karlsson <j.a.c.karlsson@student.vu.nl>
2018-11-16 14:21:59 +0100

    Fixed lab 1.2

--- Interaktionsprogrammering/app/src/main/java/com/example/jasmi/lab1/MainActivity.java
+++ Interaktionsprogrammering/app/src/main/java/com/example/jasmi/lab1/MainActivity.java
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint;
 import android.graphics.drawable.GradientDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.PasswordTransformationMethod;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
@@ -11,6 +12,7 @@ import android.widget.EditText;
 import android.widget.GridLayout;
 import android.widget.LinearLayout;
 import android.widget.RatingBar;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import static android.widget.LinearLayout.*;
@@ -55,54 +57,76 @@ public class MainActivity extends AppCompatActivity {
         myLayout.setColumnCount(2);
         myLayout.setRowCount(4);
 
-
         EditText nameField = new EditText(this);
         nameField.setMaxLines(1);
-        nameField.setWidth(500);
+        nameField.setWidth(myLayout.getWidth());
+        nameField.setText("Anders");
 
         EditText passField = new EditText(this);
         passField.setMaxLines(1);
+        passField.setText("password");
+        passField.setTransformationMethod(PasswordTransformationMethod.getInstance());
 
         EditText emailField = new EditText(this);
         emailField.setMaxLines(1);
+        emailField.setText("Anders.froberg@liu.se");
 
         TextView name = new TextView(this);
         name.setText("Namn");
+        name.setTextSize(16);
 
         TextView password = new TextView(this);
         password.setText("Lösenord");
+        password.setTextSize(16);
 
         TextView email = new TextView(this);
         email.setText("Epost");
+        email.setTextSize(16);
 
         TextView age = new TextView(this);
         age.setText("Ålder");
+        age.setTextSize(16);
 
-        myLayout.addView(name,new GridLayout.LayoutParams(
+        SeekBar ageBar = new SeekBar(this);
+
+        GridLayout.LayoutParams firstCol = new GridLayout.LayoutParams(
                 GridLayout.spec(0, GridLayout.CENTER),
-                GridLayout.spec(0, GridLayout.CENTER)));
+                GridLayout.spec(0, GridLayout.LEFT));
+        firstCol.setMargins(0,0,120,0);
+
+        myLayout.addView(name,firstCol);
+
 
         myLayout.addView(nameField, new GridLayout.LayoutParams(
                 GridLayout.spec(0, GridLayout.LEFT),
-                GridLayout.spec(1, GridLayout.LEFT)));
+                GridLayout.spec(1, GridLayout.FILL)));
 
         myLayout.addView(password, new GridLayout.LayoutParams(
                 GridLayout.spec(1, GridLayout.CENTER),
-                GridLayout.spec(0, GridLayout.CENTER)));
+                GridLayout.spec(0, GridLayout.LEFT)));
+
         myLayout.addView(passField, new GridLayout.LayoutParams(
                 GridLayout.spec(1, GridLayout.LEFT),
-                GridLayout.spec(1, GridLayout.LEFT)));
+                GridLayout.spec(1, GridLayout.FILL)));
 
         myLayout.addView(email, new GridLayout.LayoutParams(
                 GridLayout.spec(2, GridLayout.CENTER),
-                GridLayout.spec(0, GridLayout.CENTER)));
+                GridLayout.spec(0, GridLayout.LEFT)));
+
         myLayout.addView(emailField, new GridLayout.LayoutParams(
                 GridLayout.spec(2, GridLayout.LEFT),
-                GridLayout.spec(1, GridLayout.LEFT)));
+                GridLayout.spec(1, GridLayout.FILL)));
 
         myLayout.addView(age, new GridLayout.LayoutParams(
-                GridLayout.spec(3, GridLayout.CENTER),
-                GridLayout.spec(0, GridLayout.CENTER)));
+                GridLayout.spec(3, GridLayout.LEFT),
+                GridLayout.spec(0, GridLayout.LEFT)));
+
+        GridLayout.LayoutParams ageParam = new GridLayout.LayoutParams(
+                GridLayout.spec(3, GridLayout.LEFT),
+                GridLayout.spec(1, GridLayout.FILL));
+        ageParam.setMargins(0, 30, 0, 0);
+
+        myLayout.addView(ageBar, ageParam);
 
         return myLayout;
 
